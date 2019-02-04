308f4f91727b9118cb1eeec84447d04bb8892795
Shogo <pineapplehunter.daniel@gmail.com>
2019-02-04 21:04:02 +0900

    Vuepressのバージョンを上げた Markdownの表示の設定を次のとおりに変更した。  * .mdの中の改行がレンダリング後も改行になる  * リンクを()[]で囲わなくてもリンクになる

--- page/.vuepress/config.js
+++ page/.vuepress/config.js
@@ -2,9 +2,17 @@ module.exports = {
     title: "学生たちの技術ブログ",
     themeConfig: {
         nav: [
-            {text: "Home", link: "/"},
-            {text: "Posts", link: "/post/"}
+            { text: "Home", link: "/" },
+            { text: "Posts", link: "/post/" }
         ],
         sidebar: "auto"
+    },
+    markdown: {
+        extendMarkdown: md => {
+            md.set({
+                breaks: true,
+                linkify: true,
+            })
+        }
     }
 }
