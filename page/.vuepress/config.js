8f04ea8282cb8939971023c26f2d9052be06c4e5
Shogo <pineapplehunter.daniel@gmail.com>
2019-02-04 23:07:44 +0900

    Google Analytics!

--- page/.vuepress/config.js
+++ page/.vuepress/config.js
@@ -1,3 +1,12 @@
+const google_analytics = () => {
+    return [["script", {async:true, src:"https://www.googletagmanager.com/gtag/js?id=UA-103597604-2"}],
+    ["script",{},`window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-103597604-2');` ]]
+}
+
 module.exports = {
     title: "学生たちの技術ブログ",
     themeConfig: {
@@ -14,5 +23,8 @@ module.exports = {
                 linkify: true,
             })
         }
-    }
+    },
+    head:[
+        ...google_analytics()
+    ]
 }
