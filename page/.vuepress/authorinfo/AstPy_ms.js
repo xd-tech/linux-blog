463f3d1b863d5cd2e26147d3b8debaeecbcce6c8
astpy <sketdance551@gmail.com>
2019-03-06 19:13:16 +0900

    add a article 'sushida.md'

---
+++ page/.vuepress/authorinfo/AstPy_ms.js
@@ -0,0 +1,7 @@
+export default {
+  username: "AstPy_ms",
+  name: "AstPy_ms",
+  bio: "このブログの音ゲーマー部門部長券Linuxにわか部門名誉部長です",
+  pic: "/imgs/astpy/jpg",
+  infoPageUrl: "#"
+}
--- page/.vuepress/authorinfo/index.js
+++ page/.vuepress/authorinfo/index.js
@@ -1,7 +1,11 @@
+// import USERNAME from './USERNAME'
 import Pineapplehunter from './pineapplehunter'
+import AstPy_ms from './AstPy_ms'
 
 export default {
   authors: [
+    // USERNAME
     Pineapplehunter,
+    AstPy_ms,
   ]
 }
