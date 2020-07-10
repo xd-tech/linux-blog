c57f1086708e0b46fb1164c1e03864fbbb7a73f0
Shogo Takata <pineapplehunter.daniel@gmail.com>
2020-07-10 18:10:37 +0900

    [fix]一部著者が表示されないバグを直した

--- page/.vuepress/theme/components/AuthorInfo.vue
+++ page/.vuepress/theme/components/AuthorInfo.vue
@@ -12,32 +12,35 @@
 </template>
 
 <script>
-import AuthorInfos from '../../authorinfo'
+import AuthorInfos from "../../authorinfo";
 
 export default {
   computed: {
-    show () {
-      return this.authorName !== undefined && this.authors.includes(this.authorName)
+    show() {
+      return (
+        this.authorName !== undefined &&
+        this.authors.includes(this.authorName.toLowerCase())
+      );
     },
-    authorName () {
-      return this.$page.frontmatter.author
+    authorName() {
+      return this.$page.frontmatter.author;
     },
-    authors () {
-      return AuthorInfos.authors.map(x => x.username)
+    authors() {
+      return AuthorInfos.authors.map(x => x.username.toLowerCase());
     },
-    info () {
-      return AuthorInfos
-        .authors
-        .filter(x => x.username === this.authorName)[0]
+    info() {
+      return AuthorInfos.authors.filter(
+        x => x.username.toLowerCase() === this.authorName.toLowerCase()
+      )[0];
     }
   }
-}
+};
 </script>
 
 <style lang="css" scoped>
 .info-container {
   display: flex;
-  align-items:center;
+  align-items: center;
   margin: 20px;
   background-color: rgba(200, 200, 200, 0.5);
   border-radius: 10px;
