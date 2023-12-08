91304d27476e87ab068f4f72a6cd34e86647a0cd
Shogo Takata <peshogo@gmail.com>
2023-12-09 04:28:56 +0900

    nixについての記事追加

--- flake.nix
+++ flake.nix
@@ -6,6 +6,14 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
+      packages.x86_64-linux = rec {
+        default = dev;
+        dev = pkgs.writeShellScriptBin "dev" ''
+          #!${pkgs.bash}/bin/bash
+          ${pkgs.bun}/bin/bun install
+          ${pkgs.bun}/bin/bun run dev
+        '';
+      };
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs;[ git bun ];
       };
