43a16e5bd9dffd01c3f756bda15d176d7d8960cc
Shogo Takata <peshogo@gmail.com>
2025-03-11 00:35:35 +0900

    update deps

--- flake.nix
+++ flake.nix
@@ -1,21 +1,54 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }:
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      inherit (nixpkgs)lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = system: import nixpkgs { inherit system; };
     in
     {
-      packages.x86_64-linux = rec {
-        default = dev;
-        dev = pkgs.writeShellScriptBin "dev" ''
-          #!${pkgs.bash}/bin/bash
-          ${pkgs.bun}/bin/bun install
-          ${pkgs.bun}/bin/bun run dev
-        '';
-      };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs;[ git bun ];
-      };
+      packages = eachSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+          bun = lib.getExe pkgs.bun;
+        in
+        rec {
+          default = dev;
+          dev = pkgs.writeShellScriptBin "dev" ''
+            ${bun} install
+            ${bun} run dev
+          '';
+          build = pkgs.writeShellScriptBin "build" ''
+            ${bun} install
+            ${bun} run build
+          '';
+        }
+      );
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              bun
+              self.packages.${system}.dev
+              self.packages.${system}.build
+            ];
+          };
+        }
+      );
     };
 }
